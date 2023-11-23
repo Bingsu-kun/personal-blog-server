2b87490ec16008eadd078c47271b76b964235029
Bingsu-kun <icetime963@gmail.com>
2023-11-23 18:24:01 +0900

    Feat: ArticleRepository

--- src/main/kotlin/com/personal/blog/article/ArticleRepository.kt
+++ src/main/kotlin/com/personal/blog/article/ArticleRepository.kt
@@ -1,12 +1,9 @@
 package com.personal.blog
 
-import kotlin.collections.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository
 
+interface ArticleRepository : MongoRepository<Article, String> {
 
-interface ArticleRepository: MongoRepository<Article, String> {
-
-    List<Article> findByTitle(String title)
-    List<Article> findByTag(String Tag) 
+    fun findByTitle(title: String): Iterable<Article>
+    fun findByTag(tag: String): Iterable<Article>
 }
