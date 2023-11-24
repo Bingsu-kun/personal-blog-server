01eb326a370e04a33387552d046ee6f5403a14b2
Bingsu-kun <icetime963@gmail.com>
2023-11-24 17:51:00 +0900

    Feat: create test

--- src/test/kotlin/com/personal/blog/ArticleControllerTests.kt
+++ src/test/kotlin/com/personal/blog/ArticleControllerTests.kt
@@ -1,5 +1,6 @@
 package com.personal.blog
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -10,7 +11,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
-class ArticleControllerTests(@Autowired val mockMvc: MockMvc) {
+class ArticleControllerTests(
+        @Autowired val mockMvc: MockMvc,
+        @Autowired val objectMapper: ObjectMapper
+) {
 
     @MockBean lateinit var articleRepository: ArticleRepository
 
@@ -23,7 +27,7 @@ class ArticleControllerTests(@Autowired val mockMvc: MockMvc) {
 
         // When
         mockMvc.perform(
-                        MockMvcRequestBuilders.get("/api/v1/articles/65605d2055c8d990f6057163")
+                        MockMvcRequestBuilders.get("/api/v1/articles/65605d2055c8d990f6057163/")
                                 .accept(MediaType.APPLICATION_JSON)
                 )
                 // Then
@@ -32,5 +36,55 @@ class ArticleControllerTests(@Autowired val mockMvc: MockMvc) {
                 .andExpect(jsonPath("\$.[0].title").value(simpleArticle.title))
                 .andExpect(jsonPath("\$.[0].content").value(simpleArticle.content))
                 .andExpect(jsonPath("\$.[0].tags").value(simpleArticle.tags))
+    }
+
+    @Test
+    fun `Get articles`() {
+        // Given
+        val simpleArticle1 = Article(null, "제목1", "내용1", listOf("Test"), "썸넬Url입니다")
+        val simpleArticle2 = Article(null, "제목2", "내용2", listOf("Test"), "썸넬Url입니다")
+        val simpleArticle3 = Article(null, "제목3", "내용3", listOf("Test"), "썸넬Url입니다")
+
+        articleRepository.save(simpleArticle1)
+        articleRepository.save(simpleArticle2)
+        articleRepository.save(simpleArticle3)
+
+        // When
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get("/api/v1/articles/")
+                                .accept(MediaType.APPLICATION_JSON)
+                )
+                // Then
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("\$.[0].title").value(simpleArticle1.title))
+                .andExpect(jsonPath("\$.[1].title").value(simpleArticle2.title))
+                .andExpect(jsonPath("\$.[2].title").value(simpleArticle3.title))
+    }
+
+    @Test
+    fun `Create article`() {
+        // Given
+        val simpleArticle = Article(null, "제목", "내용", listOf("Test"), "썸넬Url입니다")
+
+        // When
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/api/v1/articles/")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(simpleArticle)) // JSON 화
+                )
+                .andExpect(status().isOk)
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("\$.[0].title").value(simpleArticle.title))
+                .andExpect(jsonPath("\$.[0].content").value(simpleArticle.content))
+                .andExpect(jsonPath("\$.[0].tags").value(simpleArticle.tags))
+
+        // Then
+        val savedArticle = articleRepository.findAll().get(0)
+
+        assert(simpleArticle.title == savedArticle.title)
+        assert(simpleArticle.content == savedArticle.content)
+        assert(simpleArticle.tags == savedArticle.tags)
+        assert(simpleArticle.thumbnail == savedArticle.thumbnail)
     }
 }
