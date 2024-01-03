65c3abf0fd1bca72bebc97eb0fa746928f7b3fb3
bingsu-kun <icetime963@gmail.com>
2024-01-03 09:40:28 +0900

    Revert "Feat: dockerfile"
    This reverts commit 91f39855f05c917230cd39258b4316e387f5a0c5.

--- bin/main/com/personal/blog/article/dto/ReplaceArticleDto.kt
+++
@@ -1,6 +0,0 @@
-data class ReplaceArticleDto(
-        val title: String,
-        val content: String,
-        val tags: List<String>,
-        val thumbnail: String?
-)
--- bin/main/com/personal/blog/article/dto/UpdateArticleDto.kt
+++
@@ -1,6 +0,0 @@
-data class UpdateArticleDto(
-                val title: String?,
-                val content: String?,
-                val tags: List<String>?,
-                val thumbnail: String?
-)
